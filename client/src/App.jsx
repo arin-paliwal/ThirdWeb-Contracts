2dda1b2820173570fd577f2fe89a5727e2b7dca4
Arin Paliwal <paliwalarin09@gmail.com>
2023-08-26 21:40:24 +0530

    Added Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,21 @@
-export default function Home() {
+import React from 'react'
+import { Routes,Route, BrowserRouter } from 'react-router-dom'
+import { CampaignDetails,CreateCampaign,Home,Profile } from './pages'
+const App = () => {
   return (
-    <main>
-      App
-    </main>
-  );
+    <BrowserRouter>
+    <div className='flex p-8'>
+      <div className='sm:flex hidden mr-10 relative'>
+        Sidebar
+      </div>
+      <div className='flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5'>
+        navbar
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+        </Routes>
+      </div>
+    </div></BrowserRouter>
+  )
 }
+
+export default App
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import {BrowserRouter as Router} from "react-router-dom"
-import { ChainId,ThirdwebProvider } from "@thirdweb-dev/react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import { Sepolia } from "@thirdweb-dev/chains";
 import App from "./App";
 import "./styles/globals.css";
-
-// This is the chain your dApp will work on.
-// Change this to the chain your app is built for.
-// You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-// const activeChain = "ethereum";
-
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
